f6da583d848c0cf03642fd0b3744aad54153f8da
SamHeutmaker <samheutmaker@gmail.com>
2016-03-17 21:37:20 -0700

    Added distelli-manifest

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ const searchRouter = require(__dirname + '/routes/search-routes');
 // const events12 = require(__dirname + '/util/scrapers/events12');
 // events12();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, authentication, authorization, token');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
+
 // Set Auth Routes
 app.use('/auth', mRouter);
 // Event Routes
